b7a6050ff8cbc0edb6b7959efc13c8c61191058f
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-06-24 19:11:15 +0900

    Spring Provider<T> 예제 추가.

---
+++ spring-experiments/src/test/java/kr/debop/spring/tests/SpringConfiguration.java
@@ -0,0 +1,15 @@
+package kr.debop.spring.tests;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * kr.debop.spring.tests.SpringConfiguration
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 13. 6. 24. 오후 7:03
+ */
+@Configuration
+@ComponentScan(basePackages="kr.debop.spring.tests")
+public class SpringConfiguration {
+}
---
+++ spring-experiments/src/test/java/kr/debop/spring/tests/provider/ProviderTest.java
@@ -0,0 +1,61 @@
+package kr.debop.spring.tests.provider;
+
+import kr.debop.spring.tests.SpringConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Provider;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * kr.debop.spring.tests.provider.ProviderTest
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 13. 6. 24. 오후 7:02
+ */
+@Slf4j
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration( classes = { SpringConfiguration.class } )
+public class ProviderTest {
+
+    @Autowired
+    SingletonBean singletonBeanA;
+    @Autowired
+    SingletonBean singletonBeanB;
+
+    @Test
+    public void singletonBean() {
+        assertThat(singletonBeanA).isEqualTo(singletonBeanB);
+    }
+
+    @Test
+    public void providerBean() {
+        PrototypeBean prototypeBeanA = singletonBeanA.getPrototypeBean();
+        PrototypeBean prototypeBeanB = singletonBeanB.getPrototypeBean();
+
+        assertThat(prototypeBeanA).isNotEqualTo(prototypeBeanB);
+    }
+}
+
+@Service
+class SingletonBean {
+
+    @Autowired Provider<PrototypeBean> prototypeBeanProvider;
+
+    PrototypeBean getPrototypeBean() {
+        return prototypeBeanProvider.get();
+    }
+}
+
+@Service
+@Scope("prototype")
+class PrototypeBean {
+
+}
