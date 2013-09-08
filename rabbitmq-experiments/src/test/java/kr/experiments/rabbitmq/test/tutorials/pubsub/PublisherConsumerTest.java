be89b71481e571221f9bd964290483da824cb31f
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-09-08 18:08:52 +0900

    RabbitMQ Tutorial 작성 (Publisher/Consumer)

---
+++ rabbitmq-experiments/src/test/java/kr/experiments/rabbitmq/test/tutorials/pubsub/PublisherConsumerTest.java
@@ -0,0 +1,97 @@
+package kr.experiments.rabbitmq.test.tutorials.pubsub;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * kr.experiments.rabbitmq.test.tutorials.pubsub.PublisherConsumerTest
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 13. 9. 8. 오후 6:06
+ */
+@Slf4j
+public class PublisherConsumerTest {
+    EmitLog emitLog;
+    ReceiveLogs receiveLog;
+    ReceiveLogs receiveLog2;
+
+    @Before
+    public void before() throws IOException {
+        emitLog = new EmitLog();
+        emitLog.connectQueue();
+
+        receiveLog = new ReceiveLogs();
+        receiveLog.connectQueue();
+
+        receiveLog2 = new ReceiveLogs();
+        receiveLog2.connectQueue();
+    }
+
+    @After
+    public void after() throws IOException {
+        emitLog.close();
+        receiveLog.close();
+        receiveLog2.close();
+    }
+
+    @Test
+    public void publishConsumerTest() throws Exception {
+
+        final String taskMessage = "Log Message ";
+        Thread taskThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                try {
+                    while (true) {
+                        Thread.sleep(10);
+                        emitLog.send(taskMessage + i++);
+                    }
+                } catch (InterruptedException e) {
+                    // ignore
+                } catch (IOException e) {
+                    log.warn("데이터 보내기에 실패했습니다.", e);
+                }
+            }
+        });
+        taskThread.start();
+
+        Thread workerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    receiveLog.receive();
+                } catch (InterruptedException e) {
+                    // ignore
+                } catch (IOException e) {
+                    log.warn("데이터 받기에 실패했습니다.", e);
+                }
+            }
+        });
+        workerThread.start();
+
+        Thread workerThread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    receiveLog.receive();
+                } catch (InterruptedException e) {
+                    // ignore
+                } catch (IOException e) {
+                    log.warn("데이터 받기에 실패했습니다.", e);
+                }
+            }
+        });
+        workerThread2.start();
+
+        Thread.sleep(1000);
+
+        taskThread.interrupt();
+        workerThread.interrupt();
+        workerThread2.interrupt();
+    }
+}
---
+++ rabbitmq-experiments/src/test/java/kr/experiments/rabbitmq/test/tutorials/pubsub/ReceiveLogs.java
@@ -0,0 +1,56 @@
+package kr.experiments.rabbitmq.test.tutorials.pubsub;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.StringUtils;
+
+import java.io.IOException;
+
+/**
+ * kr.experiments.rabbitmq.test.tutorials.pubsub.ReceiveLogs
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 13. 9. 8. 오후 6:01
+ */
+@Slf4j
+public class ReceiveLogs {
+
+    private final static String EXCHANGE_NAME = "logs";
+
+    private Connection connection;
+    private Channel channel;
+    private String queueName;
+
+    public void connectQueue() throws IOException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        log.debug(" [*] Waiting for messages...");
+    }
+
+
+    public void receive() throws IOException, InterruptedException {
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(queueName, true, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = StringUtils.newStringUtf8(delivery.getBody());
+            log.debug(" [x] Received = [{}]", message);
+        }
+    }
+
+    public void close() throws IOException {
+        channel.close();
+        connection.close();
+    }
+}
